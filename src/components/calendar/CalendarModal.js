777b70ef0a03da44ad76d6cb5099b726e3d6250c
ercardona <ercardona@edrans.com>
2021-08-27 21:27:08 +0200

    modal validations

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import DateTimePicker from "react-datetime-picker";
+import moment from "moment";
+import Swal from "sweetalert2";
 
 const customStyles = {
   content: {
@@ -13,16 +16,76 @@ const customStyles = {
 };
 Modal.setAppElement("#root");
 
+const now = moment().minutes(0).seconds(0).add(1, "hours");
+const nowPlus1 = now.clone().add(1, "hours");
+
 export const CalendarModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [dateStart, setDateStart] = useState(now.toDate());
+  const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
+  const [titleValid, setTitleValid] = useState(true);
+
+  const [formValues, setFormValues] = useState({
+    title: "Evento",
+    notes: "",
+    start: now.toDate(),
+    end: nowPlus1.toDate(),
+  });
+
+  const { notes, title, start, end } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
 
   const closeModal = () => {
-    setIsOpen(false);
+    //TODO: cerrar modal
+  };
+
+  const handleStartDateChange = (e) => {
+    setDateStart(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
+  };
+
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const momentStart = moment(start);
+    const momentEnd = moment(end);
+
+    if (momentStart.isSameOrAfter(momentEnd)) {
+      Swal.fire(
+        "Error",
+        "La fecha de fin debe de ser mayor que la fecha de inicio",
+        "error"
+      );
+      return;
+    }
+
+    if (title.trim().length < 2) {
+      return setTitleValid(false);
+    }
+
+    //TODO: realizar la persistencia en bd
+    setTitleValid(true);
+    closeModal();
   };
 
   return (
     <Modal
-      isOpen={isOpen}
+      isOpen={true}
       style={customStyles}
       onRequestClose={closeModal}
       className="modal"
@@ -30,9 +93,65 @@ export const CalendarModal = () => {
       closeTimeoutMS={200}
       //   ariaHideApp={ !process.env.NODE_ENV === 'test' }
     >
-      <h1>Hola Mundo</h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <span>Hola de nuevo</span>
+      <form className="container" onSubmit={handleSubmitForm}>
+        <div className="form-group">
+          <label>Fecha y hora inicio</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Fecha y hora fin</label>
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={dateEnd}
+            minDate={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <hr />
+        <div className="form-group">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className={`form-control ${!titleValid && "is-invalid"}`}
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={title}
+            onChange={handleInputChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={notes}
+            onChange={handleInputChange}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };
